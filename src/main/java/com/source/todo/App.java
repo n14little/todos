e08871fa3f95dcec9c5c8c866013d413d5778f46
Nick Little <n14little@gmail.com>
2018-06-13 23:06:44 -0500

    maven archetype output + maven.compiler update

---
+++ src/main/java/com/source/todo/App.java
@@ -0,0 +1,13 @@
+package com.source.todo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
